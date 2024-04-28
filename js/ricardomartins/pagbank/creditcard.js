063867c258f380212db6ca7655c4ca467b622310
Ligia Salzano <ligiasalzano@gmail.com>
2024-04-28 16:21:21 -0300

    If you can't find the telephone input, look for the fax input #187480753

--- js/ricardomartins/pagbank/creditcard.js
+++ js/ricardomartins/pagbank/creditcard.js
@@ -351,6 +351,7 @@ RMPagBank.prototype = {
         let name = quote.customerName ? quote.customerName : $$('input[name^="billing[firstname]').first().value + ' ' + $$('input[name^="billing[lastname]').first().value;
         let phone = quote.phone.replace(/\D/g, '');
         phone = phone ? phone : $$('input[name^="billing[telephone]').first().value.replace(/\D/g, '');
+        phone = phone ? phone : $$('input[name^="billing[fax]').first().value.replace(/\D/g, '');
         let street = quote.street ? quote.street : $$('input[name^="billing[street]').first().value;
         let number = quote.number ? quote.number : $$('input[name^="billing[street]')[1].value;
         let complement = quote.complement ? quote.complement : quote.neighborhood;
